b42b53449f71285822c92d43e419ee9fb594fe20
Christopher Huaman <chuaman@britanico.edu.pe>
2024-11-10 22:34:00 -0500

    que el boton de eliminar cuenta funcione cuando esta rellenado todo

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Mail\AccountDeletedMail;
+use App\Models\HealthSheet;
+use App\Models\SheetNutritional;
+use App\Models\Travel;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
@@ -48,13 +51,28 @@ class ProfileController extends Controller
         ]);
 
         $user = $request->user();
-	Mail::to($user->email)->send(new AccountDeletedMail($user));
+
+        //Guardado de variables para el envió de correo posterior a la eliminación del usuario
+        $email=$user->email;
+        $email_deleted=new AccountDeletedMail($user);
+
         Auth::logout();
 
+        //No existe relacion por modelo :(
+        //Todo: Cambiar la verificación del modelo eliminando por cascada en relación polimorfica :D
+
+        //Eliminación por búsqueda de usuario
+        HealthSheet::where('userID',$user->id)->delete();
+        SheetNutritional::where('userID',$user->id)->delete();
         $user->delete();
+
+        Mail::to($email)->send($email_deleted);
+
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+
+
 
         return Redirect::to('/');
     }
