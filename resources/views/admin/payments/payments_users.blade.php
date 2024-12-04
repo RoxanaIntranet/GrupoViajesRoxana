0c3e9ba4a674bada00c42179ad489ba8112da356
Christopher Huaman <chuaman@britanico.edu.pe>
2024-12-03 22:33:48 -0500

    cambios de quotas

--- resources/views/admin/payments/payments_users.blade.php
+++ resources/views/admin/payments/payments_users.blade.php
@@ -17,14 +17,14 @@
                 <tbody>
                 @foreach($group_user as $passenger)
                     <tr class="bg-white hover:bg-gray-100">
-                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->travelID}}</td>
-                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->tipo_grupo}}</td>
-                        <td class="border border-gray-300 px-4 py-2">{{$passenger->nombre_grupo}}</td>
-                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->tipo_encargado}}</td>
-                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->nombre_encargado}}</td>
-                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->telefono_encargado}}</td>
+                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->group->travelID}}</td>
+                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->group->tipo_grupo}}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{$passenger->group->nombre_grupo}}</td>
+                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->group->tipo_encargado}}</td>
+                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->group->nombre_encargado}}</td>
+                        <td class="border border-gray-300 px-4 py-2 text-center">{{$passenger->group->telefono_encargado}}</td>
                         <td class="border border-gray-300 px-4 py-2 text-center">
-                            <a href="{{route('payments_admin.index.payments_users_quota',$passenger->travelID)}}">
+                            <a href="{{route('payments_admin.index.payments_users_quota',[$passenger->id])}}">
                                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                                     <i class="fas fa-pen">Cuota</i>
                                 </button>
